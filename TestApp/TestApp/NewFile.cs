9f31b162bcaed6aa173ba60e25a0a5e58e9e0ed1
Florieta <florieta.pencheva@gmail.com>
2022-11-04 16:08:14 +0200

    Add a new file

---
+++ TestApp/TestApp/NewFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    internal class NewFile
+    {
+        public int Id { get; set; }
+    }
+}
